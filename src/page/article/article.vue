7371dd8617351364720ee27285185d60e332b5c6
bigy <bigy6241@gmail.com>
2017-06-24 14:13:01 +0800

    样式添加

--- src/page/article/article.vue
+++ src/page/article/article.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="wrapper">
     <view-box ref="viewBox">
-      <div class="content" v-html="body"></div>
+      <div class="news-content" v-html="body"></div>
     </view-box>
   </div>
 </template>
 <script>
-import { ViewBox } from 'vux'
+import {
+  ViewBox
+} from 'vux'
 import Vue from 'vue'
 export default {
   components: {
@@ -24,17 +26,30 @@ export default {
     Vue.http.get('/api/4/news/' + to.params.article_id)
       .then(function(res) {
         let _body = res.data
+        let _head = document.getElementsByTagName('head')[0]
+        _body.css.push('http://daily.zhihu.com/css/share.css?v=5956a')
+        _body.css.forEach(function(item) {
+          let _cssEle = document.createElement('link')
+          _cssEle.rel = "stylesheet"
+          _cssEle.href = item
+          console.log(_cssEle)
+          _head.appendChild(_cssEle)
+        })
+        let _$headline =
+          `<div class="img-wrap">
+                <h1 class="headline-title">${_body.title}</h1>
+                <span class="img-source">图片${_body.image_source}</span>
+                <img src="${_body.image}" alt="">
+                <div class="img-mask"></div>
+            </div>`
         next(vm => {
           vm.body = _body.body
           vm.image = _body.image
           vm.title = _body.title
           vm.copyRight = vm.copyRight.concat(_body.recommenders)
-          let _head = document.getElementsByTagName('head')[0]
-          _body.css.forEach(function(item) {
-            let _cssEle = document.createElement('link')
-            _cssEle.href = item
-            console.log(_cssEle)
-            _head.appendChild(_cssEle)
+          vm.$nextTick(function() {
+            document.querySelector('.headline').innerHTML = _$headline
+            console.log(document.querySelector('.headline'))
           })
         })
       })
